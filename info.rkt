0ce6f433aa46f69ea2c4a27f4651e15b6bef19ee
Jens Axel Søgaard <jensaxel@soegaard.net>
2026-03-11 21:12:30 +0100

    Add MIT license metadata to package info

--- info.rkt
+++ info.rkt
@@ -3,6 +3,7 @@
 (define collection 'multi)
 (define pkg-desc "Scribble helpers for CSS, HTML, JavaScript, and Scribble code")
 (define version "0.2.0")
+(define license 'MIT)
 (define deps '("base" "scribble-lib" "syntax-color-lib"))
 (define build-deps '("scribble-doc" "racket-doc"))
 (define scribblings '(("scribblings/scribble-tools.scrbl" () (library))))
