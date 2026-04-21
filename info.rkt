91f894d7d2b0882d8bfa5dce511550b2657f145a
Jens Axel Søgaard <jensaxel@soegaard.net>
2026-04-21 10:02:55 +0200

    Declare lexers subpackage dependencies

--- info.rkt
+++ info.rkt
@@ -1,9 +1,14 @@
 #lang setup/infotab
 
 (define collection "scribble-tools")
-(define pkg-desc "Scribble helpers for CSS, HTML, JavaScript, Python, and Scribble code")
+(define pkg-desc "Scribble helpers for CSS, HTML, JavaScript, Python, shell, WebAssembly, and related code")
 (define version "0.2")
 (define license 'MIT)
-(define deps '("base" "scribble-lib" "syntax-color-lib" "lexers"))
+(define deps '("base"
+               "scribble-lib"
+               "syntax-color-lib"
+               "lexers"
+               "lexers-lib"
+               "parser-tools-lib"))
 (define build-deps '("scribble-doc" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/scribble-tools.scrbl" () (library))))
